98172f5a08994178e9884244b102d741fb02648f
Dmitry Aloyan <lead@w.wilix.ru>
2017-11-02 12:27:16 +0300

    add delete username method; add patch username method; add two functions: find index, find value; add adding username; add view all username-array;

--- index.js
+++ index.js
@@ -6,22 +6,68 @@ app.use(express.static('html'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-  if (req.method === 'GET') {
-    console.log('Request Type:', req.method);
-    next();
+app.post('/user/update', (req, res, next) => {
+  var index = findIndexArr(req.body.oldUsName);
+  if (index !== -1) {
+    users[index] = req.body.newUsName;
+    res.send(users.join(', '));
   } else {
-    console.log('Method are not GET');
+    res.send('user not found');
   }
-}, (req, res, next) => {
-  console.log('Request URL:', req.originalUrl);
-  next();
 });
 
-app.use((req, res) => {
-  res.send('ok');
+var users = ['john', 'joooohhhnnn', 'heli, a.a'];
+var user, findIndx;
+// app.use((req, res, next) => {
+//   if (req.method === 'GET') {
+//     console.log('Request Type:', req.method);
+//     next();
+//   } else {
+//     console.log('Method are not GET');
+//   }
+// }, (req, res, next) => {
+//   console.log('Request URL:', req.originalUrl);
+//   next();
+// });
+
+
+app.get('/user/getall', (req, res, next) => {
+  console.log(users.join(', '));
+  res.send(users.join(', '));
 });
+
+app.put('/user/add/:username', (req, res, next) => {
+  var findElem = findElement(user);
+  user = req.params.username;
+  findIndx = findIndexArr(user);
+  console.log(findElem);
+  if (findElem === user) { res.send('error! name used'); } else {
+    users.push(user);
+    console.log(users.join(', '));
+    res.send('success');
+  }
+});
+
+function findElement(userName) {
+  return users.find((item, index, arr) => {
+    return item === userName;
+  });
+}
+
+function findIndexArr(userName) {
+  
+  return users.findIndex((item, index, arr) => {
+    return item === userName;
+  });
+}
+
+app.delete('/user/del/:username', (req, res, next) => {
+  users.splice(findIndx, 1);
+  res.send({ success: false, data: users });
+});
+
+
 
 app.listen(3000, () => {
   console.log('server listening on 3000 port...');
-});
+}); 
